4b78dd1f53f4c2f73315a6ed2ad1c32cae243ec8
404notfound <1420812669@qq.com>
2024-12-25 20:08:59 +0800

    create hid file

---
+++ main/usb_hid.c
@@ -0,0 +1,9 @@
+#include "usb_hid.h"
+#include "main.h"
+
+void usb_hid_init(void) {
+    // 初始化USB HID设备
+    // 设置USB设备描述符
+    // 设置端点描述符
+    // 设置中断请求
+}
---
+++ main/usb_hid.h
@@ -0,0 +1,4 @@
+#ifndef __USB_HID_H__
+#define __USB_HID_H__
+
+#endif  // __USB_HID_H__
